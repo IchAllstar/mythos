1532adf1cef78b58cfd38350b1797a5976e084ea
Stefan Bonfert <stefan.bonfert@bonfert-online.de>
2017-02-16 15:27:59 +0100

    Restricted the EC benchmarks to one run, because of the limited number of Capability pointers available

--- kernel/app/init-app/app/init.cc
+++ kernel/app/init-app/app/init.cc
@@ -199,124 +199,117 @@ void localKernelObjectLatency(){
 void executionContextCreationLatencyBundled(){
 
 	mythos::PortalFutureRef<void> res1 = mythos::PortalFutureRef<void>(myPortal);
-	for (size_t run = 0; run < NUM_RUNS; run++){
-		uint64_t start, end;
-		start = getTime();
-		//Create new ECs for all HWTs besides the current one
-		for (size_t worker = 0; worker < AVAILABLE_HWTS - 1; worker++){
-			//Create a new invocation buffer
-			mythos::Frame new_msg_ptr(mythos::init::APP_CAP_START+1+worker*3+(run*AVAILABLE_HWTS*3));
-			res1 = new_msg_ptr.create(res1.reuse(), kmem, mythos::init::MEMORY_REGION_FACTORY, 1<<21, 1<<21);
-			res1.wait();
-			ASSERT(res1.state() == mythos::Error::SUCCESS);
+	uint64_t start, end;
+	start = getTime();
+	//Create new ECs for all HWTs besides the current one
+	for (size_t worker = 0; worker < AVAILABLE_HWTS - 1; worker++){
+		//Create a new invocation buffer
+		mythos::Frame new_msg_ptr(mythos::init::APP_CAP_START+1+worker*3);
+		res1 = new_msg_ptr.create(res1.reuse(), kmem, mythos::init::MEMORY_REGION_FACTORY, 1<<21, 1<<21);
+		res1.wait();
+		ASSERT(res1.state() == mythos::Error::SUCCESS);
 
-			//Map the invocation buffer frame into user space memory
-			mythos::InvocationBuf* ib = (mythos::InvocationBuf*)(((11+worker)<<21));
+		//Map the invocation buffer frame into user space memory
+		mythos::InvocationBuf* ib = (mythos::InvocationBuf*)(((11+worker)<<21));
 
-			auto res3 = myAS.mmap(res1.reuse(), new_msg_ptr, (uintptr_t)ib, 1<<21, mythos::protocol::PageMap::MapFlags().writable(true));
-			res3.wait();
-			ASSERT(res3.state() == mythos::Error::SUCCESS);
+		auto res3 = myAS.mmap(res1.reuse(), new_msg_ptr, (uintptr_t)ib, 1<<21, mythos::protocol::PageMap::MapFlags().writable(true));
+		res3.wait();
+		ASSERT(res3.state() == mythos::Error::SUCCESS);
 
-			//Create a second portal
-			mythos::Portal portal2(mythos::init::APP_CAP_START+2+worker*3+(run*AVAILABLE_HWTS*3), ib);
-			res1 = portal2.create(res3.reuse(), kmem, mythos::init::PORTAL_FACTORY);
-			res1.wait();
-			ASSERT(res1.state() == mythos::Error::SUCCESS);
+		//Create a second portal
+		mythos::Portal portal2(mythos::init::APP_CAP_START+2+worker*3, ib);
+		res1 = portal2.create(res3.reuse(), kmem, mythos::init::PORTAL_FACTORY);
+		res1.wait();
+		ASSERT(res1.state() == mythos::Error::SUCCESS);
 
-			//Create a second EC on its own HWT
-			mythos::ExecutionContext ec(mythos::init::APP_CAP_START+3+worker*3+(run*AVAILABLE_HWTS*3));
-			res1 = ec.create(res1.reuse(), kmem, mythos::init::EXECUTION_CONTEXT_FACTORY,
-					myAS, myCS, mythos::init::SCHEDULERS_START+1+worker,
-					initstack_top+stacksize-4096*worker, &thread_main, (void*)worker);
-			res1.wait();
-			ASSERT(res1.state() == mythos::Error::SUCCESS);
+		//Create a second EC on its own HWT
+		mythos::ExecutionContext ec(mythos::init::APP_CAP_START+3+worker*3);
+		res1 = ec.create(res1.reuse(), kmem, mythos::init::EXECUTION_CONTEXT_FACTORY,
+				myAS, myCS, mythos::init::SCHEDULERS_START+1+worker,
+				initstack_top+stacksize-4096*worker, &thread_main, (void*)worker);
+		res1.wait();
+		ASSERT(res1.state() == mythos::Error::SUCCESS);
 
-			//Bind the new portal to the new EC
-			res1 = portal2.bind(res1.reuse(), new_msg_ptr, 0, mythos::init::APP_CAP_START+3+worker*3+(run*AVAILABLE_HWTS*3));
-			res1.wait();
-			ASSERT(res1.state() == mythos::Error::SUCCESS);
-		}
-
-		//Run all created ECs
-		for (size_t worker = 0; worker < AVAILABLE_HWTS - 1; worker++){
-			mythos::ExecutionContext ec(mythos::init::APP_CAP_START+3+worker*3+(run*AVAILABLE_HWTS*3));
-			res1 = ec.run(res1.reuse());
-			res1.wait();
-			ASSERT(res1.state() == mythos::Error::SUCCESS);
-		}
-		end = getTime();
-		if (end < start){
-			run--;
-			continue;
-		}
-		MLOG_ERROR(mlog::app, "Create+Run of ", AVAILABLE_HWTS-1, "HWTs: ", end - start);
+		//Bind the new portal to the new EC
+		res1 = portal2.bind(res1.reuse(), new_msg_ptr, 0, mythos::init::APP_CAP_START+3+worker*3);
+		res1.wait();
+		ASSERT(res1.state() == mythos::Error::SUCCESS);
 	}
+
+	//Run all created ECs
+	for (size_t worker = 0; worker < AVAILABLE_HWTS - 1; worker++){
+		mythos::ExecutionContext ec(mythos::init::APP_CAP_START+3+worker*3);
+		res1 = ec.run(res1.reuse());
+		res1.wait();
+		ASSERT(res1.state() == mythos::Error::SUCCESS);
+	}
+	MLOG_ERROR(mlog::app, "Create+Run of ", AVAILABLE_HWTS-1, "HWTs: ", end - start);
 }
 
 void executionContextCreationLatencySeparate(){
 	mythos::PortalFutureRef<void> res1 = mythos::PortalFutureRef<void>(myPortal);
-	for (size_t run = 0; run < NUM_RUNS; run++){
-				uint64_t start, mid, end;
 
-				//Create new ECs for all HWTs besides the current one
-				for (size_t worker = 0; worker < AVAILABLE_HWTS - 1; worker++){
-					start = getTime();
-					//Create a new invocation buffer
-					mythos::Frame new_msg_ptr(mythos::init::APP_CAP_START+1+worker*3+(run*AVAILABLE_HWTS*3));
-					res1 = new_msg_ptr.create(res1.reuse(), kmem, mythos::init::MEMORY_REGION_FACTORY, 1<<21, 1<<21);
-					res1.wait();
-					ASSERT(res1.state() == mythos::Error::SUCCESS);
+	uint64_t start, mid, end;
 
-					//Map the invocation buffer frame into user space memory
-					mythos::InvocationBuf* ib = (mythos::InvocationBuf*)(((11+worker)<<21));
+	//Create new ECs for all HWTs besides the current one
+	for (size_t worker = 0; worker < AVAILABLE_HWTS - 1; worker++){
+		start = getTime();
+		//Create a new invocation buffer
+		mythos::Frame new_msg_ptr(mythos::init::APP_CAP_START+1+worker*3);
+		res1 = new_msg_ptr.create(res1.reuse(), kmem, mythos::init::MEMORY_REGION_FACTORY, 1<<21, 1<<21);
+		res1.wait();
+		ASSERT(res1.state() == mythos::Error::SUCCESS);
 
-					auto res3 = myAS.mmap(res1.reuse(), new_msg_ptr, (uintptr_t)ib, 1<<21, mythos::protocol::PageMap::MapFlags().writable(true));
-					res3.wait();
-					ASSERT(res3.state() == mythos::Error::SUCCESS);
+		//Map the invocation buffer frame into user space memory
+		mythos::InvocationBuf* ib = (mythos::InvocationBuf*)(((11+worker)<<21));
 
-					//Create a second portal
-					mythos::Portal portal2(mythos::init::APP_CAP_START+2+worker*3+(run*AVAILABLE_HWTS*3), ib);
-					res1 = portal2.create(res3.reuse(), kmem, mythos::init::PORTAL_FACTORY);
-					res1.wait();
-					ASSERT(res1.state() == mythos::Error::SUCCESS);
+		auto res3 = myAS.mmap(res1.reuse(), new_msg_ptr, (uintptr_t)ib, 1<<21, mythos::protocol::PageMap::MapFlags().writable(true));
+		res3.wait();
+		ASSERT(res3.state() == mythos::Error::SUCCESS);
 
-					//Create a second EC on its own HWT
-					mythos::ExecutionContext ec(mythos::init::APP_CAP_START+3+worker*3+(run*AVAILABLE_HWTS*3));
-					res1 = ec.create(res1.reuse(), kmem, mythos::init::EXECUTION_CONTEXT_FACTORY,
-							myAS, myCS, mythos::init::SCHEDULERS_START+1+worker,
-							initstack_top+stacksize-4096*worker, &thread_main, (void*)worker);
-					res1.wait();
-					ASSERT(res1.state() == mythos::Error::SUCCESS);
+		//Create a second portal
+		mythos::Portal portal2(mythos::init::APP_CAP_START+2+worker*3, ib);
+		res1 = portal2.create(res3.reuse(), kmem, mythos::init::PORTAL_FACTORY);
+		res1.wait();
+		ASSERT(res1.state() == mythos::Error::SUCCESS);
 
-					//Bind the new portal to the new EC
-					res1 = portal2.bind(res1.reuse(), new_msg_ptr, 0, mythos::init::APP_CAP_START+3+worker*3+(run*AVAILABLE_HWTS*3));
-					res1.wait();
-					ASSERT(res1.state() == mythos::Error::SUCCESS);
+		//Create a second EC on its own HWT
+		mythos::ExecutionContext ec(mythos::init::APP_CAP_START+3+worker*3);
+		res1 = ec.create(res1.reuse(), kmem, mythos::init::EXECUTION_CONTEXT_FACTORY,
+				myAS, myCS, mythos::init::SCHEDULERS_START+1+worker,
+				initstack_top+stacksize-4096*worker, &thread_main, (void*)worker);
+		res1.wait();
+		ASSERT(res1.state() == mythos::Error::SUCCESS);
 
-					mid = getTime();
+		//Bind the new portal to the new EC
+		res1 = portal2.bind(res1.reuse(), new_msg_ptr, 0, mythos::init::APP_CAP_START+3+worker*3);
+		res1.wait();
+		ASSERT(res1.state() == mythos::Error::SUCCESS);
 
-					res1 = ec.run(res1.reuse());
-					res1.wait();
-					ASSERT(res1.state() == mythos::Error::SUCCESS);
-					end = getTime();
-					if (mid < start || end < mid){
-						worker--;
-						continue;
-					}
-					MLOG_ERROR(mlog::app, "Create on HWT ", worker+1, ": ", mid - start, " Run: ", end - mid);
-				}
-			}
+		mid = getTime();
+
+		res1 = ec.run(res1.reuse());
+		res1.wait();
+		ASSERT(res1.state() == mythos::Error::SUCCESS);
+		end = getTime();
+		if (mid < start || end < mid){
+			worker--;
+			continue;
+		}
+		MLOG_ERROR(mlog::app, "Create on HWT ", worker+1, ": ", mid - start, " Run: ", end - mid);
+	}
+
 }
 
 void benchmarks(){
 	//invocation latency to mobile kernel object
-	mobileKernelObjectLatency();
+	//mobileKernelObjectLatency();
 
 	//invocation latency to local kernel object
 	//localKernelObjectLatency();
 
 	//latency of creating and starting up execution contexts
-	//executionContextCreationLatencyBundled();
+	executionContextCreationLatencyBundled();
 	//executionContextCreationLatencySeparate();
 }
 
