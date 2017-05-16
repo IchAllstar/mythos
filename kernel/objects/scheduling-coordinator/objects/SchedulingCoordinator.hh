a0aa9c0d6799cc0bbee82f431331c364e987b8ed
Stefan <hertrste@b-tu.de>
2017-05-16 15:52:52 +0200

    Introduced THreadGroup object for every core. SchedulingCoordinator supports sleep policy and sends sleep intents. ThreadGroup goes to sleep together, does not wakeup together yet.

--- kernel/objects/scheduling-coordinator/objects/SchedulingCoordinator.hh
+++ kernel/objects/scheduling-coordinator/objects/SchedulingCoordinator.hh
@@ -25,7 +25,7 @@
  */
 #pragma once
 
-#include "async/NestedMonitorDelegating.hh"
+#include "async/NestedMonitorHome.hh"
 #include "cpu/CoreLocal.hh"
 #include "objects/ISchedulable.hh"
 #include "objects/IKernelObject.hh"
@@ -36,71 +36,153 @@
 
 namespace mythos {
 
+using mythos::async::Place;
+using mythos::SchedulingContext;
+
+class CoreGroup;
+
 /**
  * Class coordinates between Place(kernel task scheduler), SchedulingContext(system thread scheduler),
  * ExecutionContext (system thread) and the decision to go to sleep. Different policies of sleeping can be chosen.
  */
 class SchedulingCoordinator
-  : public IKernelObject {
+    : public IKernelObject {
 
-  enum Policy {
-    SLEEP = 0,
-    SPIN  = 1,
-  };
+    enum Policy {
+        SLEEP = 0,
+        SPIN,
+        GROUP,
+    };
+
+ friend class CoreGroup;
 
 //IKernelObject interface
 public:
-  optional<void> deleteCap(Cap self, IDeleter& del) override;
-  void invoke(Tasklet* t, Cap self, IInvocation* msg) override;
-  optional<void const*> vcast(TypeId id) const override;
+
+    optional<void> deleteCap(Cap self, IDeleter& del) override;
+    void invoke(Tasklet* t, Cap self, IInvocation* msg) override;
+    optional<void const*> vcast(TypeId id) const override;
 
 //protocol implementations
 protected:
-  friend struct protocol::SchedulingCoordinator;
-  Error printMessage(Tasklet *t, Cap self, IInvocation *msg);
-  Error setPolicy(Tasklet *t, Cap self, IInvocation *msg);
+    friend struct protocol::SchedulingCoordinator;
+    Error printMessage(Tasklet *t, Cap self, IInvocation *msg);
+    Error setPolicy(Tasklet *t, Cap self, IInvocation *msg);
 
-  friend struct protocol::KernelObject;
-  Error getDebugInfo(Cap self, IInvocation* msg);
+    friend struct protocol::KernelObject;
+    Error getDebugInfo(Cap self, IInvocation* msg);
 
 
 // Actual Methods
 public:
-  NORETURN void runUser() {
-    switch (policy) {
-      case SLEEP : runSleep();
-      case SPIN  : runSpin();
-      default    : runSleep();
+    NORETURN void runUser() {
+        switch (policy) {
+            case SLEEP : runSleep(); break;
+            case SPIN  : runSpin();  break;
+            case GROUP : runGroup(); break;
+            default    : runSleep(); break;
+        }
     }
-  }
 
-  NORETURN void sleep() {
-    MLOG_DETAIL(mlog::boot, "going to sleep now");
-    mythos::cpu::go_sleeping(); // resets the kernel stack!
-  }
+    NORETURN void sleep() {
+        MLOG_DETAIL(mlog::boot, "going to sleep now");
+        mythos::cpu::go_sleeping(); // resets the kernel stack!
+    }
 
-  NORETURN void runSleep();
-  NORETURN void runSpin();
+    void init(mythos::async::Place *p, mythos::SchedulingContext *sc, CoreGroup *core_) {
+        //mlogsc.error("init", p , sc);
+        ASSERT(p != nullptr);
+        ASSERT(sc != nullptr);
+        localPlace = p;
+        localSchedulingContext = sc;
+        core = core_;
+        monitor.setHome(p);
+    }
 
-  void init(mythos::async::Place *p, mythos::SchedulingContext *sc) {
-    //mlogsc.error("init", p , sc);
-    ASSERT(p != nullptr);
-    ASSERT(sc != nullptr);
-    localPlace = p;
-    localSchedulingContext = sc;
-    monitor.setHome(p);
-  }
-
+    NORETURN void runSleep();
+    NORETURN void runSpin();
+    NORETURN void runGroup();
 private:
-  // kernel object stuff
-  IDeleter::handle_t del_handle = {this};
-  async::NestedMonitorDelegating monitor;
+    // kernel object stuff
+    IDeleter::handle_t del_handle = {this};
+    async::NestedMonitorHome monitor;
 
-  // actual stuff
-  mythos::async::Place *localPlace = nullptr;
-  mythos::SchedulingContext *localSchedulingContext = nullptr;
+    // actual stuff
+    Place *localPlace = nullptr;
+    SchedulingContext *localSchedulingContext = nullptr;
 
-  Policy policy = {SLEEP};
+    void (*runPolicy)(int);
+
+    Policy policy = {GROUP};
+
+    CoreGroup *core {nullptr};
+
+    volatile bool SLEEP_FLAG = false;
+};
+
+class CoreGroup {
+private:
+    static const constexpr int HWTHREADS_PER_CORE = 2;
+    SchedulingCoordinator* group[HWTHREADS_PER_CORE] = {nullptr};
+    bool intents[HWTHREADS_PER_CORE] = {false};
+public:
+
+    void sleep_intent(int apicID) {
+
+    }
+
+    void sleep_unintent(int apicID) {
+
+    }
+
+    bool full() {
+        for (int i = 0; i < HWTHREADS_PER_CORE; i++) {
+            if (group[i] == nullptr) {
+                return false;
+            }
+        }
+        MLOG_ERROR(mlog::boot, "FULL");
+        return true;
+    }
+
+    bool contains(SchedulingCoordinator *sc) {
+        for (int i = 0; i < HWTHREADS_PER_CORE; i++) {
+            if (sc == group[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void group_sleep() {
+        for (int i = 0; i < HWTHREADS_PER_CORE; i++) {
+            group[i]->SLEEP_FLAG = true;
+        }
+    }
+
+    void sleep_intent(SchedulingCoordinator *sc) {
+        if (!contains(sc)) {
+            MLOG_ERROR(mlog::boot, "NEW Sleep intent", DVAR(sc));
+            for (int i = 0; i < HWTHREADS_PER_CORE; i++) {
+                if (group[i] == nullptr) {
+                    group[i] = sc;
+                    break;
+                }
+            }
+            if (full()) {
+                group_sleep();
+            }
+        }
+    }
+
+    void sleep_unintent(SchedulingCoordinator *sc) {
+        for (int i = 0; i < HWTHREADS_PER_CORE; i++) {
+            if (group[i] == sc) {
+                MLOG_ERROR(mlog::boot, "Sleep Unintent", DVAR(sc));
+                group[i] = nullptr;
+            }
+        }
+    }
 };
 
 } // namespace mythos
